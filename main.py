762d298bbd78095ecb5d3f4cd77b98ffb342b798
Danilo Quattrini <daniloquattrini.com@gmail.com>
2025-09-22 00:05:13 +0200

    feat: complete the core concept of the game Add the function to check if the user is within the range of the number wrote and change the function to the random module from randrange to randint.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
 
-
 def define_random_number(min_limiter: int, max_limiter: int) -> int:
-    return random.randrange(min_limiter, max_limiter)
+    return random.randint(min_limiter, max_limiter)
 
-min_range, max_range= int(input("Define the min range\n==>")), int(input("Define the max range\n==>"))
-define_random_number(min_range, max_range)
+def guess_game(number_to_guess: int):
+    chosen_number: int = 0
+    guess_counter: int = 0
+    while chosen_number != number_to_guess:
+        chosen_number = int(input("Guess a number ==> "))
+        if chosen_number > number_to_guess: print("Too high")
+        else: print("Too low")
+        guess_counter += 1
+    print(f"You Win!!!\nTotal guess: {guess_counter}")
+
+min_range, max_range = int(input("Define the min range\n==>")), int(input("Define the max range\n==>"))
+guess_game(define_random_number(min_range, max_range))
